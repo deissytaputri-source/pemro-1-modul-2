dec809b9a1bb4450ea594c96847a9d2a9d362464
deissytaputri <deissytaputri@gmail.com>
2025-10-24 01:11:20 +0800

    Update file C terbaru

--- Modul2_C/PRAK305-2510817320010-PutriDeissytaHerliyana.c
+++ Modul2_C/PRAK305-2510817320010-PutriDeissytaHerliyana.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int input;
     scanf("%d", &input);
 
     int hari = 0, jam = 0, menit = 0, detik = 0;
 
-    hari = input / 86400;
+    hari = input / 86400;            // 1 hari = 86400 detik
+    jam = (input % 86400) / 3600;    // sisa hari dibagi 3600 detik = jam
+    menit = (input % 3600) / 60;     // sisa jam dibagi 60 detik = menit
+    detik = input % 60;              // sisa terakhir = detik
 
-    jam = (input % 86400)/3600;
-
-    menit = (input % 3600)/60;
-
-    detik = input % 60;
-
-    if(hari == 0){
-        ptintf("%.2d:%.2d:%.2d", jam, menit, detik);
-    } else{
-        printf("%d hari %.2d:%.2d:%.2d", hari, jam, menit, detik);
+    if (hari == 0) {
+        printf("%.2d:%.2d:%.2d\n", jam, menit, detik);
+    } else {
+        printf("%d hari %.2d:%.2d:%.2d\n", hari, jam, menit, detik);
     }
 
-    return 0;
+    return 0;
 }
