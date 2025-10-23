001b4ce3a0e4149e4b1a1f87502bc693c6cfc77f
deissytaputri <deissytaputri@gmail.com>
2025-10-24 01:07:15 +0800

    Update file C terbaru

---
+++ Modul2_C/PRAK304-2510817320010-PutriDeissytaHerliyana.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int nilai;
+    scanf("%d", &nilai);
+
+    if (nilai == 0) {
+        printf("Nol\n");
+    } else if (nilai < 10) {
+        printf("Satuan\n");
+    } else if (nilai < 20) {
+        printf("Belasan\n");
+    } else if (nilai < 100) {
+        printf("Puluhan\n");
+    } else {
+        printf("Anda Menginput Melebihi Limit Bilangan\n");
