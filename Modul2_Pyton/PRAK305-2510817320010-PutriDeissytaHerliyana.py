7baf32f8cf6402744285a10bbacaab1577f253ff
deissytaputri <deissytaputri@gmail.com>
2025-10-25 22:52:58 +0800

    save

--- Modul2_Pyton/PRAK305-2510817320010-PutriDeissytaHerliyana.py
+++ Modul2_Pyton/PRAK305-2510817320010-PutriDeissytaHerliyana.py
@@ -1,11 +1,11 @@
-inputdetik = int(input())
+input_detik = int(input())
 
-hari = inputdetik // 86400
-jam = (inputdetik % 3600) // 3600
-menit = (inputdetik % 3600) // 60
-detik = inputdetik % 60
+hari = input_detik // 86400
+jam = (input_detik % 86400) // 3600
+menit = (input_detik % 3600) // 60
+detik = input_detik % 60
 
 if hari == 0:
- print(f"{jam:02d}:{menit:02d}:{detik:02d}")
+    print(f"{jam:02d}:{menit:02d}:{detik:02d}")
 else:
     print(f"{hari} hari {jam:02d}:{menit:02d}:{detik:02d}")
